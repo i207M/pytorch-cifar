13d2a9d889aa843a985f617774497848eb606329
i207m <i207m@outlook.com>
2021-10-17 00:01:15 +0800

    missing randomCrop!

--- main-original.py
+++ main-original.py
@@ -32,6 +32,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 print('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, 4),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -42,12 +43,12 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data2', train=True, download=True, transform=transform_train
+    root='./data2', train=True, download=True, transform=transform_train, pin_memory=True
 )
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data2', train=False, download=True, transform=transform_test
+    root='./data2', train=False, download=True, transform=transform_test, pin_memory=True
 )
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
 
